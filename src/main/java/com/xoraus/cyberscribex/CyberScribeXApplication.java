9f7b50503278006feaa090b988066338064cda66
xoraus <imsafay@gmail.com>
2024-04-07 20:49:06 +0530

    initial commit

---
+++ src/main/java/com/xoraus/cyberscribex/CyberScribeXApplication.java
@@ -0,0 +1,13 @@
+package com.xoraus.cyberscribex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberScribeXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberScribeXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xoraus/cyberscribex/CyberScribeXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoraus.cyberscribex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberScribeXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
