12cda893d4f831b7c70a4b626c8e651a00adf4bd
xoraus <imsafay@gmail.com>
2024-04-09 12:38:21 +0530

    Adds Post

---
+++ src/main/java/com/xoraus/cyberscribex/controller/PostController.java
@@ -0,0 +1,52 @@
+package com.xoraus.cyberscribex.controller;
+
+import com.xoraus.cyberscribex.payload.PostDto;
+import com.xoraus.cyberscribex.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create blog post
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    // get all Posts
+    @GetMapping
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+    // get post by id
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name ="id") long id){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    // update post by id
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name ="id") long id){
+        PostDto updatedPost = postService.updatePost(postDto, id);
+        return new ResponseEntity<>(updatedPost, HttpStatus.OK);
+    }
+
+    // delete post by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name ="id") long id){
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post deleted successfully", HttpStatus.OK);
+    }
+}
