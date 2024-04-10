03bbb447a1bed89899eb2418acfa146ef0350cdd
xoraus <imsafay@gmail.com>
2024-04-10 23:31:14 +0530

    Code Refactoring

--- src/main/java/com/xoraus/cyberscribex/payload/PostDto.java
+++ src/main/java/com/xoraus/cyberscribex/payload/PostDto.java
@@ -3,10 +3,13 @@ package com.xoraus.cyberscribex.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
