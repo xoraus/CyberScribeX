55dc788ffecb62e06333723e5436be210efdeeff
xoraus <imsafay@gmail.com>
2024-04-12 21:31:31 +0530

    Creates CustomUserDetailsService

---
+++ src/main/java/com/xoraus/cyberscribex/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.xoraus.cyberscribex.security;
+
+import com.xoraus.cyberscribex.entity.User;
+import com.xoraus.cyberscribex.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException("User doest not exit with this username or email: " + usernameOrEmail));
+
+        Set<GrantedAuthority> authoritySet = user
+                .getRoles()
+                .stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authoritySet);
+    }
+}
