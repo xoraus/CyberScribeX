a92d19c816439e59bf08e0d841f591e31129c748
xoraus <imsafay@gmail.com>
2024-04-16 16:01:24 +0530

    Adds Category REST APIs

---
+++ src/main/java/com/xoraus/cyberscribex/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.xoraus.cyberscribex.repository;
+
+import com.xoraus.cyberscribex.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
--- src/main/java/com/xoraus/cyberscribex/repository/PostRepository.java
+++ src/main/java/com/xoraus/cyberscribex/repository/PostRepository.java
@@ -3,7 +3,8 @@ package com.xoraus.cyberscribex.repository;
 import com.xoraus.cyberscribex.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-
+    List<Post> findByCategoryId(Long categoryId);
 }
